a6e3e223033a53633e39d692d369202821cec669
ujnzxw <zhaoxiaowei.cn@hotmail.com>
2017-03-09 23:07:05 +0800

    format code for more readable

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ from config import xfce_displays
 
 
 def set_background(file_path):
+    '''
+    set desktop background
+    '''
     de = get_desktop_environment()
 
     if de in ["gnome", "unity", "cinnamon", "pantheon", "gnome-classic"]:
@@ -47,27 +50,34 @@ def set_background(file_path):
     return True
 
 
-# http://stackoverflow.com/a/21213358/4466589
 def get_desktop_environment():
-    # From http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
-    # and http://ubuntuforums.org/showthread.php?t=652320
-    # and http://ubuntuforums.org/showthread.php?t=652320
-    # and http://ubuntuforums.org/showthread.php?t=1139057
+    '''
+    From http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
+    and http://ubuntuforums.org/showthread.php?t=652320
+    and http://ubuntuforums.org/showthread.php?t=652320
+    and http://ubuntuforums.org/showthread.php?t=1139057
+    and http://stackoverflow.com/a/21213358/4466589
+    '''
     if sys.platform in ["win32", "cygwin"]:
         return "windows"
     elif sys.platform == "darwin":
         return "mac"
-    else:  # Most likely either a POSIX system or something not much common
+    ''' Most likely either a POSIX system or something not much common '''
+    else:
         desktop_session = os.environ.get("DESKTOP_SESSION")
-        if desktop_session is not None:  # Easier to match if we don't have to deal with caracter cases
+        ''' Easier to match if we don't have to deal with caracter cases '''
+        if desktop_session is not None:
             desktop_session = desktop_session.lower()
-            if desktop_session in ["gnome", "unity", "cinnamon", "mate", "xfce4", "lxde", "fluxbox",
-                                   "blackbox", "openbox", "icewm", "jwm", "afterstep", "trinity", "kde", "pantheon",
-                                   "gnome-classic", "i3"]:
+            if desktop_session in ["gnome",   "unity", "cinnamon", "mate",
+                                   "xfce4",   "lxde" , "fluxbox" , "blackbox",
+                                   "openbox", "icewm", "jwm",      "afterstep",
+                                   "trinity", "kde",   "pantheon", "gnome-classic", "i3"]:
                 return desktop_session
-            ## Special cases ##
-            # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE if using LXDE.
-            # There is no guarantee that they will not do the same with the other desktop environments.
+            '''
+            Special cases
+            Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE if using LXDE.
+            There is no guarantee that they will not do the same with the other desktop environments.
+            '''
             elif "xfce" in desktop_session or desktop_session.startswith("xubuntu"):
                 return "xfce4"
             elif desktop_session.startswith("ubuntu"):
@@ -76,7 +86,7 @@ def get_desktop_environment():
                 return "lxde"
             elif desktop_session.startswith("kubuntu"):
                 return "kde"
-            elif desktop_session.startswith("razor"):  # e.g. razorkwin
+            elif desktop_session.startswith("razor"):   # e.g. razorkwin
                 return "razor-qt"
             elif desktop_session.startswith("wmaker"):  # e.g. wmaker-common
                 return "windowmaker"
@@ -85,20 +95,20 @@ def get_desktop_environment():
         elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
             if "deprecated" not in os.environ.get('GNOME_DESKTOP_SESSION_ID'):
                 return "gnome2"
-        # From http://ubuntuforums.org/showthread.php?t=652320
+        ''' From http://ubuntuforums.org/showthread.php?t=652320 '''
         elif is_running("xfce-mcs-manage"):
             return "xfce4"
         elif is_running("ksmserver"):
             return "kde"
 
-    # We couldn't detect it so far, so let's try one last time
+    ''' We couldn't detect it so far, so let's try one last time '''
     current_desktop = os.environ.get("XDG_CURRENT_DESKTOP")
     if current_desktop:
         current_desktop = current_desktop.lower()
         if current_desktop in ["gnome", "unity", "kde", "gnome-classic", "mate"]:
             return current_desktop
 
-        # Special Cases
+        ''' Special Cases '''
         elif current_desktop == "xfce":
             return "xfce4"
         elif current_desktop == "x-cinnamon":
@@ -108,6 +118,7 @@ def get_desktop_environment():
 
 
 def has_program(program):
+    ''' check if the program is existed '''
     try:
         subprocess.check_output(["which", "--", program])
         return True
